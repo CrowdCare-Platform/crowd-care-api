3a76d08395923a9fbf36253186fadc55cf06c784
Axel Lemmens <lemmensaxel@gmail.com>
2025-08-11 15:49:40 +0200

    updates to device feedback endpoint

---
+++ prisma/migrations/20250811134858_removed_memmory/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `totalMemory` on the `DeviceFeedback` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DeviceFeedback" DROP COLUMN "totalMemory";
